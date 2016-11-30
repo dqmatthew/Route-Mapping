90892608cc87caf53f15f9f52b9753c1e1fb1370
Matthew Deng <15madeng@gmail.com>
2016-11-30 01:02:33 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,11 +1,5 @@
 import java.util.ArrayList;
 
-/* Author: Matthew Deng
- * Course: CSC 172
- * Lab Session: MW 1525-1640
- * Project 4
- * Date: April 13, 2016
- */
 public class Node { // intersections
 	public double latitude, longitude;
 	public String NodeID;
